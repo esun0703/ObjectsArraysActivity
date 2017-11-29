726966392f1fee0cbbf2882194a6abd86034eb30
Emily Sun <emilysun@Emilys-MacBook-Pro.local>
2017-11-29 15:37:18 -0500

    reordered commands

--- index.js
+++ index.js
@@ -20,30 +20,41 @@
         const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
     // and we want to make a copy of it.
-        // const team = players;
-        // console.log(team, players);
+    // You might think we can just do something like this:
+
+        const team = players;
+        console.log(team, players);
+        
+
+    // however what happens when we update that array?
         team[3] = 'Lux';
+        
+
+    // now here is the problem!
         // index 3 of team is Lux
         // index 3 of players is also Lux;
         // important note because this means that team actually is players and changes it.
-
-    // You might think we can just do something like this:
-
-    // however what happens when we update that array?
-
-    // now here is the problem!
+        // if you reference an array it updates the original array
 
     // oh no - we have edited the original array too!
 
     // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
     // So, how do we fix this? We take a copy instead!
-
     // one way
+        const team2 = players.slice(); //if you pass nothing will just give a copy of of the array
+        team2[3];
+        console.log(team2);
+        console.log(players);
 
     // or create a new array and concat the old one in
+        const team3 = [].concat(players);
+        console.log(team3);
 
     // or use the new ES6 Spread
+        const team4 = [...players];
+        team4[3] = 'blah blah';
+        console.log(team4);
 
     // now when we update it, the original one isn't changed
 
