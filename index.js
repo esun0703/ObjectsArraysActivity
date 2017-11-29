523f33389595d6d30099af3efd8fdb65f6bb5ad2
Emily Sun <emilysun@Emilys-MacBook-Pro.local>
2017-11-29 15:42:44 -0500

    added more console.logs

--- index.js
+++ index.js
@@ -18,16 +18,19 @@
 
     // Let's say we have an array
         const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+        console.log(players);
 
     // and we want to make a copy of it.
     // You might think we can just do something like this:
 
         const team = players;
-        console.log(team, players);
+        // console.log(team, players);
         
 
     // however what happens when we update that array?
         team[3] = 'Lux';
+        console.log(team);
+        console.log(players);
         
 
     // now here is the problem!
@@ -43,18 +46,22 @@
     // So, how do we fix this? We take a copy instead!
     // one way
         const team2 = players.slice(); //if you pass nothing will just give a copy of of the array
-        team2[3];
+        team2[3] = "whoo";
         console.log(team2);
         console.log(players);
 
     // or create a new array and concat the old one in
         const team3 = [].concat(players);
+        team3[3] = 'lala'
         console.log(team3);
 
     // or use the new ES6 Spread
         const team4 = [...players];
         team4[3] = 'blah blah';
         console.log(team4);
+
+        const team5 = Array.from(players);
+        console.log(team5);
 
     // now when we update it, the original one isn't changed
 
